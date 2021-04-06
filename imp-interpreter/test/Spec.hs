3ede52da8ad0ef9b7029dd1969a298b075f45ed6
Yuxi Liu <ubikium@gmail.com>
2021-04-06 23:05:12 +0900

    add tests

--- imp-interpreter/test/Spec.hs
+++ imp-interpreter/test/Spec.hs
@@ -1,10 +1,13 @@
 {-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings     #-}
 import           IMP.Pattern
 import           IMP.Semantics
 import           IMP.Syntax
 
+import           Data.Bifunctor         (first)
 import qualified Data.IntMap            as M
+import qualified Data.Text              as T
 import           Data.Tuple             (swap)
 import           Numeric.Natural
 
@@ -20,7 +23,8 @@ main = defaultMain tests
 tests :: TestTree
 tests = testGroup "Tests"
   [ -- properties
-   unitTests
+    unitTests
+  , functionalTests
   ]
 
 properties :: TestTree
@@ -38,9 +42,7 @@ scProps = testGroup "(checked by SmallCheck)"
   ]
 
 unitTests = testGroup "Unit tests"
-  [ testCase "test sum" $
-      assertBool (show . interpret $ sumPgm) False
-  , testCase "test ruleSeqCompose" $
+  [ testCase "test ruleSeqCompose" $
       ruleSeqCompose (sc $ SSeq SUnit SUnit) @?= Just (sc SUnit)
   , testCase "test ruleWhile" $
       ruleWhile (sc $ SWhile (BLit True) Nothing) @?= Just (sc $ SIte (BLit True) (Just $ SSeq (SBlock Nothing)
@@ -48,7 +50,16 @@ unitTests = testGroup "Unit tests"
   , testCase "test ruleVarLookup" $
       ruleVarLookup (Config (SAssign 1 (AVar 0)) state0)  @?= Just (Config (SAssign 1 (ALit 10)) state0)
   , testCase "test ruleAssign" $
-      ruleAssign (Config (SAssign 1 (AVar 0)) state0) @?= Just (Config SUnit state1)
+      ruleAssign (Config (SAssign 1 (ALit 10)) state0) @?= Just (Config SUnit state1)
+  ]
+
+functionalTests = testGroup "Functional tests"
+  [ testCase "test collatz" . assertBool "collatz is consistant with result" $
+      collatzPgm `checkAgainst` collatzRes
+  , testCase "test prime" . assertBool "prime is consistant with result" $
+      primesPgm `checkAgainst` primesRes
+  , testCase "test sum" . assertBool "sum is consistant with result" $
+      sumPgm `checkAgainst` sumRes
   ]
 
 -- -------
@@ -71,3 +82,31 @@ state0 :: State
 state0 = M.fromList [(0, KRInt 10), (1, KRInt 0)]
 state1 :: State
 state1 = M.fromList [(0, KRInt 10), (1, KRInt 10)]
+
+checkId :: Eq v => IdMap v -> IdMap KResult -> [(v, KResult)] -> Bool
+checkId idMap state ref =
+  let indexRes = M.toList state
+      res = map (first (idMap M.!)) indexRes
+   in all (`elem` res) ref  -- assert on a subset
+
+checkAgainst :: Pgm T.Text -> [(T.Text, KResult)] -> Bool
+checkAgainst pgm ref =
+  let (idMap, cfg) = interpret pgm
+   in checkId idMap (state cfg) ref
+
+collatzRes :: [(T.Text, KResult)]
+collatzRes = [ ("s", KRInt 66)
+             , ("m", KRInt 10)
+             ]
+
+primesRes :: [(T.Text, KResult)]
+primesRes = [ ("s", KRInt 4)
+            , ("m", KRInt 10)
+            ]
+
+sumRes :: [(T.Text, KResult)]
+sumRes = [ ("n", KRInt 0)
+         , ("sum", KRInt 5050)
+         ]
+
+
